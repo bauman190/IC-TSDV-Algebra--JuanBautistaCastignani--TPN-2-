cdcf4fe01533119d902e77b2aa3be1b6b6e67b85
bautista <bautista@example.com>
2023-09-24 20:02:31 -0300

    cambio en el nombre de struct

--- TP2 Algebra/main.cpp
+++ TP2 Algebra/main.cpp
@@ -1,13 +1,13 @@
 #include "raylib.h"
 #include <math.h>
 #include <iostream>
+#include <vector>
 
-struct vector
+struct myVector
 {
     Vector3 start;
     Vector3 end;
 };
-
 
 
 int main()
@@ -15,6 +15,11 @@ int main()
     
     const int screenWidth = 1500;
     const int screenHeight = 800;
+
+    int N;
+
+    std::cout << "N por el que se divide el vector 3:" ;
+    std::cin >> N;
 
     InitWindow(screenWidth, screenHeight, "La piramide");
 
@@ -27,9 +32,7 @@ int main()
     camera.fovy = 70.0f;                                
     camera.projection = CAMERA_PERSPECTIVE;
     
-    
-
-    vector vectorA;
+    myVector vectorA;
     vectorA.start.x = 0.0f;
     vectorA.start.y = 0.0f;
     vectorA.start.z = 0.0f;
@@ -38,9 +41,7 @@ int main()
     vectorA.end.y = GetRandomValue(1,5);
     vectorA.end.z = GetRandomValue(1,5);
 
-    std::cout << vectorA.end.x << " " << vectorA.end.y << " " << vectorA.end.z;
-
-    vector vectorB;
+    myVector vectorB;
     vectorB.start.x = vectorA.start.x;
     vectorB.start.y = vectorA.start.y;
     vectorB.start.z = vectorA.start.z;
@@ -49,18 +50,18 @@ int main()
     vectorB.end.y = vectorA.end.y;
     vectorB.end.z = -vectorA.end.x;
 
-    vector vectorC;
+    myVector vectorC;
     vectorC.start.x = vectorA.start.x;
     vectorC.start.y = vectorA.start.y;
     vectorC.start.z = vectorA.start.z;
 
+    //https://www.youtube.com/watch?v=pWbOisq1MJU&list=PLkg_Ym7wbXg72cBmR0YdvZw4RbEnhzeOM&index=2&t=107s&ab_channel=TheOrganicChemistryTutor
     vectorC.end.x = ((vectorA.end.y * vectorB.end.z) - (vectorA.end.z * vectorB.end.y));
     vectorC.end.y = ((vectorA.end.z * vectorB.end.x) - (vectorA.end.x * vectorB.end.z));
     vectorC.end.z = ((vectorA.end.x * vectorB.end.y) - (vectorA.end.y * vectorB.end.x));
 
     Vector3 corte;
-
-    corte.y = vectorA.end.x / 3;
+    corte.y = vectorA.end.x / N;
     corte.x = (vectorC.end.x / vectorC.end.y) * corte.y;
     corte.z = (vectorC.end.z / vectorC.end.y) * corte.y;
 
@@ -68,7 +69,6 @@ int main()
   
     while (!WindowShouldClose())    
     {      
-    
         UpdateCamera(&camera);
 
         BeginDrawing();
@@ -83,7 +83,7 @@ int main()
 
         DrawLine3D(vectorC.start, corte, GREEN);
 
-        DrawGrid(20.0f, 1.0f);
+        DrawGrid(20.0f, 1.0f);        
         
         EndMode3D();
 
@@ -96,4 +96,10 @@ int main()
 
     return 0;
 }
+
+float magnitud(Vector3 vector) 
+{
+    return sqrt(pow(vector.x, 2) + pow(vector.y, 2) + pow(vector.z, 2));
+}
+
 
